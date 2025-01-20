bd1f82c70bd2ca17ef55f0232d2e133009bd8f7c
Tokosama <anouartoko@gmail.com>
2025-01-20 14:46:01 +0100

    Ajout de la page de dun user specifique

--- src/app/(routes)/profile/page.tsx
+++ src/app/(routes)/profile/page.tsx
@@ -1,11 +1,8 @@
 import { auth } from "@/auth";
+import ProfilePageContent from "@/components/ProfilePageContent";
 
-import ProfilePosts from "@/components/ProfilePosts";
 import { prisma } from "@/db";
-import { CheckIcon, ChevronLeft, Cog } from "lucide-react";
-import Link from "next/link";
 import { redirect } from "next/navigation";
-import { Suspense } from "react";
 export default async function ProfilePage() {
   const session = await auth();
   const profile = await prisma.profile.findFirst({
@@ -15,55 +12,6 @@ export default async function ProfilePage() {
     return redirect('/settings')
   }
   return (
-    <main>
-      <section className="flex justify-between items-center">
-        <button>
-          <ChevronLeft />
-        </button>
-        <div className="font-bold flex items-center gap-2">
-          {profile.username}
-          <div className="size-5 rounded-full bg-ig-red inline-flex justify-center items-center text-white">
-            <CheckIcon size={16} />{" "}
-          </div>
-        </div>
-        <Link href="/settings">
-          <Cog />{" "}
-        </Link>
-      </section>
-      <section className="mt-8 flex justify-center">
-        <div className="size-48 p-2  bg-gradient-to-tr from-ig-orange to-ig-red rounded-full">
-          <div className="size-44 p-2  bg-white rounded-full">
-            <div className="size-40 aspect-square overflow-hidden rounded-full">
-              <img
-                className="l"
-                src={profile.avatar || ""}
-                alt=""
-              />
-            </div>
-          </div>
-        </div>
-      </section>
-      <section className="text-center mt-2">
-        <h1 className="text-xl font-bold">{profile.name}</h1>
-        <p className="text-gray-500 mt-1 mb-1">{profile.subtitle}</p>
-        <p className="">{profile.bio}</p>
-      </section>
-      <section className="mt-4 ">
-        <div className="flex justify-center gap-4 font-bold">
-          <Link href={"/"}>Posts </Link>
-          <Link
-            className="text-gray-400"
-            href={"/highlights"}
-          >
-            Highlights
-          </Link>
-        </div>
-      </section>
-      <section className="mt-4">
-        <Suspense fallback="Loading...">
-          <ProfilePosts email={profile.email} />
-        </Suspense>
-      </section>
-    </main>
+    <ProfilePageContent profile={profile} isOurProfile={true}/>
   );
 }
